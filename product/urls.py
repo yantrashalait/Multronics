180c8f46f770a9255122343f2a7641673e30bcec
sanip <saneprijal@gmail.com>
2019-08-24 11:14:12 +0545

    Listing user waitlist, favourite, cart and notification

--- product/urls.py
+++ product/urls.py
@@ -6,8 +6,9 @@ app_name = 'product'
 urlpatterns = [
     path('', views.index, name='index'),
     path('notifications/<int:pk>/', views.view_notifications, name='notification'),
-    path('wishlists/<int:pk>/', views.view_wishlist, name='wishlist'),
+    path('wishlists/<int:pk>/', views.view_waitlist, name='wishlist'),
     path('cart/<int:pk>/', views.view_cart, name='cart'),
+    path('favourite/<int:pk>/', views.view_favourite, name='favourite'),
     path('product/list/', views.ProductList.as_view(), name='product_list'),
     path('product/add/', views.ProductAdd.as_view(), name='product_add'),
 
--- product/views.py
+++ product/views.py
@@ -6,28 +6,84 @@ def index(request):
     return render(request, 'core/index.html')
 
 
+# class NotificationList(ListView):
+#     model = Notification
+#     template_name = 'product/notification_list.html'
+#     context_object_name = 'notifications'
+
+#     def get_queryset(self, *args, **kwargs):
+#         return self.queryset.filter(user_id=kwargs.get('pk')).order_by('-date')
+
 def view_notifications(request, *args, **kwargs):
-    notifications = Notification.objects.filter(user_id=kwargs.get('pk'))
+    notifications = Notification.objects.filter(user_id=kwargs.get('pk')).order_by('-date')
     if notifications:
         return HttpResponse('notifications found')
     else:
         return HttpResponse('No notifications')
 
 
+# class CartList(ListView):
+#     model = Cart
+#     template_name = 'product/cart_list.html'
+#     context_object_name = 'carts'
+
+#     def get_queryset(self, *args, **kwargs):
+#     def get_queryset(self, *args, **kwargs):
+#         if self.request.user.is_superuser():
+#             return self.queryset.filter(removed=False).order_by('-date')
+#         else:
+#             return self.queryset.filter(user_id=kwargs.get('pk'), removed=False).order_by('-date')
+
+
 def view_cart(request, *args, **kwargs):
-    cart = Cart.objects.filter(user_id=kwargs.get('pk'))
+    if request.user.is_superuser():
+        cart = Cart.objects.filter(removed=False).order_by('-date')
+    else:
+        cart = Cart.objects.filter(user_id=kwargs.get('pk'), removed=False).order_by('date')
     if cart:
         return HttpResponse('cart found')
     else:
         return HttpResponse('no cart found')
 
 
-def view_wishlist(request, *args, **kwargs):
-    wishlist = WishList.objects.filter(user_id=kwargs.get('pk'))
-    if wishlist:
-        return HttpResponse('wishlist found')
+# class WaitListView(ListView):
+#     model = WaitList
+#     template_name = 'product/wait_list.html'
+#     context_object_name = 'waitlists'
+
+#     def get_queryset(self, *args, **kwargs):
+#         if self.request.user.is_superuser():
+#             return self.queryset.filter(removed=False).order_by('-date')
+#         else:
+#             return self.queryset.filter(user_id=kwargs.get('pk'), removed=False).order_by('-date')
+
+
+def view_waitlist(request, *args, **kwargs):
+    if request.user.is_superuser():
+        waitlist = WaitList.objects.filter(removed=False).order_by('-date')
     else:
-        return HttpResponse('no wishlist')
+        waitlist = WaitList.objects.filter(user_id=kwargs.get('pk'), removed=False).order_by('-date')
+    if wishlist:
+        return HttpResponse('waitlist found')
+    else:
+        return HttpResponse('no waitlist')
+
+
+# class FavouriteList(ListView):
+#     model = Favourite
+#     template_name = 'product/favourite_list.html'
+#     context_object_name = 'favourites'
+
+#     def get_queryset(self, *args, **kwargs):
+#         return self.queryset.filter(user_id=kwargs.get('pk'), removed=False).order_by('-date')
+
+
+def view_favourite(request, *args, **kwargs):
+    favourite = Favourite.objects.filter(user_id=kwargs.get('pk'), removed=False).order_by('-date')
+    if favourite:
+        return HttpResponse('favourite found')
+    else:
+        return HttpResponse('favourite not found')
 
 
 class ProductList(ListView):
