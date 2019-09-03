7a86722fdb8aa51eb9d2b7b13d673cad35705db8
razeevnepali <rajeev.nepali1001@gmail.com>
2019-09-03 13:15:44 +0545

    offer percent update

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -6,7 +6,7 @@ from django.forms.models import inlineformset_factory
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product 
-        fields = ['name', 'description', 'previous_price', 'new_price', 'category', 'brand', 'product_type', 'super_deals', 'offer', 'availability', 'main_image', 'color']
+        fields = ['name', 'description', 'previous_price', 'new_price', 'category', 'brand', 'product_type', 'super_deals', 'offer', 'availability', 'main_image', 'color', 'offer_percent']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
