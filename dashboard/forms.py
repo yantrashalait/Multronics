edd06aaba77be1c9061be5596e6372fbf8c4bbe3
sanip <saneprijal@gmail.com>
2019-09-11 22:16:17 +0545

    cropper

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from product.models import Product, Category, Brand, Type, BannerImage, Cart, ProductSpecification, ProductImage, SuperImage, OfferImage
 from django.forms.models import inlineformset_factory
+from PIL import Image
 
 
 class ProductForm(forms.ModelForm):
@@ -14,9 +15,27 @@ class ProductForm(forms.ModelForm):
 
 
 class CategoryForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput())
+    y = forms.FloatField(widget=forms.HiddenInput())
+    width = forms.FloatField(widget=forms.HiddenInput())
+    height = forms.FloatField(widget=forms.HiddenInput())
+
     class Meta:
         model = Category
-        fields = ['name', 'category_image']
+        fields = ['name', 'category_image', 'x', 'y', 'width', 'height']
+    
+    def save(self):
+        photo = super(CategoryForm, self).save()
+
+        x = self.cleaned_data.get('x')
+        y = self.cleaned_data.get('y')
+        w = self.cleaned_data.get('width')
+        h = self.cleaned_data.get('height')
+        # if x is not None and y is not None:
+        image = Image.open(photo.category_image)
+        cropped_image = image.crop((x, y, w+x, h+y))
+        resized_image = cropped_image.resize((410, 281), Image.ANTIALIAS)
+        resized_image.save(photo.category_image.path)
 
 
 class BrandForm(forms.ModelForm):
