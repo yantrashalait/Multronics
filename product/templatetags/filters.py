14baa86dd1f7932a1ea08f3a5124a98ee9a11f73
sanip <saneprijal@gmail.com>
2019-12-22 12:16:24 +0545

    most viewed 2 rows

--- product/templatetags/filters.py
+++ product/templatetags/filters.py
@@ -1,5 +1,5 @@
 from django.template import Library
-from product.models import Favourite, WaitList, UserBargain
+from product.models import Favourite, WaitList, UserBargain, Product
 register = Library()
 
 
@@ -28,3 +28,27 @@ def check_bargain(obj, pk):
 @register.filter
 def space_to_underscore(obj):
     return obj.replace(" ", "_")
+
+
+@register.filter
+def separate_two(popular):
+    total = len(popular)
+    if total < 2:
+        return range(1)
+    else:
+        if(total / 2) == 0:
+            return range((total / 2))
+        else:
+            return range((total // 2) + 1)
+
+
+@register.filter
+def block_viewed(popular, index=None):
+    if index == 1:
+        return Product.objects.filter(views__gte=10, visibility=True)[:2]
+
+    if index > 1:
+        start = (index - 1) * 2
+        end = start + 2
+        return Product.objects.filter(views__gte=10, visibility=True)[start:end]
+
