ff8889222d8074ecc3c380381b67de5acd266dcf
sanip <saneprijal@gmail.com>
2020-03-03 12:37:12 +0545

    header notifs fix

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -3,8 +3,8 @@ from product.models import AboutITeam, UserOrder, UserRequestProduct
 def base_processor(request):
     if request.user.is_authenticated:
         about = AboutITeam.objects.last()
-        orders = UserOrder.objects.all().order_by('date')[:3]
+        header_orders = UserOrder.objects.all().order_by('date')[:3]
         requested_products = UserRequestProduct.objects.all().order_by('date')[:3]
-        return {'about': about, 'orders': orders, 'requested_products': requested_products}
+        return {'about': about, 'header_orders': header_orders, 'requested_products': requested_products}
     else:
         return {}
